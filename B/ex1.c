720d4f6430f3e4898d95acaea52a3adcc6b4761a
Fer_Piccinini <fernandopiccinini12@gmail.com>
2024-08-26 18:43:20 -0300

    Update ex1.c

--- B/ex1.c
+++ B/ex1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
 int main(){
-
-
-
+    int x,y;
+    printf("digite dois numeros: ");
+    scanf("%d %d", &x,&y);
+    if(x>y){
+        printf("numero maior: %d, menor: %d",x,y);
+    }
+    else{
+        printf("numero maior: %d, menor: %d",y,x);
+    }
 }
